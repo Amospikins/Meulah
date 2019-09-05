07e9172376d842155d23d55011aaf20ea7a90123
Sanmi Amos <Sanmi Amos>
2019-09-05 04:07:29 +0100

    Starting Project

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+//DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', '');
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+//URL Root
+define('URLROOT', '_Your_Site_URL_');
+
+//Site Name
+define('SITENAME', '_Your_SiteName_');
